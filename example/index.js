a469ae8556f01eb8d6e31983a2e3f9f0455777ec
John Hennessey <jghennes@gmail.com>
2017-01-03 20:56:25 -0500

    changing this file extensions back .js for the time being

--- example/index.js
+++ example/index.js
@@ -30,16 +30,21 @@ class DynamicValuesForm extends React.Component {
     return (
       <div>
         <form>
-          <input type={'text'} onChange={this.handleTextInputChange} />
-          <select>
+          <label htmlFor={'string'}>String to parse:</label>
+          <input id={'string'} type={'text'} onChange={this.handleTextInputChange} />
+          <label htmlFor={'harmony'}>Color harmony:</label>
+          <select id={'harmony'} onChange={this.handleOptionInputChange}>
             <option value="complimentary">Complimentary</option>
-            <option value="split-complimentary">Split-Complimentary</option>
+            <option value="splitComplimentary">Split-Complimentary</option>
             <option value="triadic">Triadic</option>
             <option value="tetradic">Tetradic</option>
             <option value="analagous">Analagous</option>
           </select>
         </form>
-        <DynamicBackground string={this.state.stringValue} />
+        <DynamicBackground
+          string={this.state.stringValue}
+          harmony={this.state.harmonyValue}
+        />
       </div>
     );
   }
