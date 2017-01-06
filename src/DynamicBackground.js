19f2392921442a7af40edbf93aa6435f806d770c
John Hennessey <jghennes@gmail.com>
2017-01-05 20:48:54 -0500

    update string-to-colors ref to reflect package name change

--- src/DynamicBackground.js
+++ src/DynamicBackground.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { stringToColors, harmonies } from '../../string-to-colors';
+import { stringToColors, harmonies } from 'string-to-colors';
 
 const oneColorTempl = color => color;
 
